7bf17f37103d6879d10207e92d765b18a4108912
Krazy Dev <208371687+ikrazydev@users.noreply.github.com>
2025-08-31 01:00:53 +0200

    Add validation layers

--- src/app.cpp
+++ src/app.cpp
@@ -1,5 +1,23 @@
 #include "app.hpp"
 
+#include <iostream>
+
+static VkResult _createDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger) {
+    auto func = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+    if (func != nullptr) {
+        return func(instance, pCreateInfo, pAllocator, pDebugMessenger);
+    } else {
+        return VK_ERROR_EXTENSION_NOT_PRESENT;
+    }
+}
+
+static void _destroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks* pAllocator) {
+    auto func = (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
+    if (func != nullptr) {
+        func(instance, debugMessenger, pAllocator);
+    }
+}
+
 void VkImg2DApp::run()
 {
     initWindow();
@@ -23,10 +41,16 @@ void VkImg2DApp::initWindow()
 void VkImg2DApp::initVulkan()
 {
     createInstance();
+    setupDebugMessenger();
+    printExtensions();
 }
 
 void VkImg2DApp::createInstance()
 {
+    if (enableValidationLayers && !verifyValidationLayerSupport()) {
+        throw std::runtime_error("Requested validation layers are not available.");
+    }
+
     VkApplicationInfo appInfo{};
     appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
     appInfo.pApplicationName = "VkImg2D";
@@ -47,15 +71,99 @@ void VkImg2DApp::createInstance()
     std::vector<const char*> exts(glfwExts, glfwExts + glfwExtCount);
     exts.emplace_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
 
-    createInfo.enabledExtensionCount = (uint32_t)exts.size();
-    createInfo.ppEnabledExtensionNames = exts.data();
-    createInfo.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+    VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo;
+    if (enableValidationLayers) {
+        exts.emplace_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
 
-    createInfo.enabledLayerCount = 0;
+        createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+        createInfo.ppEnabledLayerNames = validationLayers.data();
+
+        populateDebugMessengerCreateInfo(debugCreateInfo);
+
+        createInfo.pNext = &debugCreateInfo;
+    } else {
+        createInfo.enabledLayerCount = 0;
+    }
+
+    createInfo.enabledExtensionCount = static_cast<uint32_t>(exts.size());
+    createInfo.ppEnabledExtensionNames = exts.data();
+
+    createInfo.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
 
     if (vkCreateInstance(&createInfo, nullptr, &mInstance) != VK_SUCCESS) {
         throw std::runtime_error("Failed to create Vulkan instance.");
     }
+}
+
+void VkImg2DApp::printExtensions()
+{
+    uint32_t extCount{0};
+    vkEnumerateInstanceExtensionProperties(nullptr, &extCount, nullptr);
+
+    std::vector<VkExtensionProperties> exts(extCount);
+    vkEnumerateInstanceExtensionProperties(nullptr, &extCount, exts.data());
+
+    std::cout << "Available extensions:\n";
+
+    for (const auto& ext : exts) {
+        std::cout << "\t" << ext.extensionName << "\n";
+    }
+}
+
+void VkImg2DApp::setupDebugMessenger()
+{
+    if (!enableValidationLayers) return;
+
+    VkDebugUtilsMessengerCreateInfoEXT createInfo;
+    populateDebugMessengerCreateInfo(createInfo);
+
+    auto result = _createDebugUtilsMessengerEXT(mInstance, &createInfo, nullptr, &mDebugMessenger);
+    if (result != VK_SUCCESS) {
+        throw std::runtime_error("Failed to set up debug messenger.");
+    }
+}
+
+void VkImg2DApp::populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo)
+{
+    createInfo = {};
+    createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+    createInfo.messageSeverity = // No info bit
+        VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT
+        | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT
+        | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+    createInfo.messageType =
+        VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT
+        | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT
+        | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+    createInfo.pfnUserCallback = debugCallback;
+    createInfo.pUserData = nullptr;
+}
+
+bool VkImg2DApp::verifyValidationLayerSupport()
+{
+    uint32_t layerCount;
+    vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+    std::vector<VkLayerProperties> vkLayers(layerCount);
+    vkEnumerateInstanceLayerProperties(&layerCount, vkLayers.data());
+
+    return std::ranges::all_of(validationLayers, [&](const char* layer) {
+        return std::ranges::any_of(vkLayers, [&](const auto& vkLayer) {
+            std::cout << std::format("Layer: {}\n", vkLayer.layerName);
+            return std::strcmp(layer, vkLayer.layerName) == 0;
+        });
+    });
+}
+
+VKAPI_ATTR VkBool32 VKAPI_CALL VkImg2DApp::debugCallback(
+    VkDebugUtilsMessageSeverityFlagBitsEXT,
+    VkDebugUtilsMessageTypeFlagsEXT,
+    const VkDebugUtilsMessengerCallbackDataEXT *pCallbackData,
+    void*)
+{
+    std::cerr << "Validation layer: " << pCallbackData->pMessage << "\n";
+
+    return VK_FALSE;
 }
 
 void VkImg2DApp::mainLoop()
@@ -67,6 +175,10 @@ void VkImg2DApp::mainLoop()
 
 void VkImg2DApp::cleanup()
 {
+    if (enableValidationLayers) {
+        _destroyDebugUtilsMessengerEXT(mInstance, mDebugMessenger, nullptr);
+    }
+
     vkDestroyInstance(mInstance, nullptr);
 
     glfwDestroyWindow(mWindow);
--- src/app.hpp
+++ src/app.hpp
@@ -1,12 +1,23 @@
 #pragma once
 
-#include <vulkan/vulkan.hpp>
+#include <vulkan/vulkan.h>
+#include <vector>
 
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
+
+const std::vector<const char*> validationLayers = {
+    "VK_LAYER_KHRONOS_validation"
+};
+
+#if DEBUG
+    const bool enableValidationLayers = true;
+#else
+    const bool enableValidationLayers = false;
+#endif
 
 class VkImg2DApp {
 public:
@@ -16,6 +27,17 @@ public:
 
     void initVulkan();
     void createInstance();
+    static void printExtensions();
+
+    void setupDebugMessenger();
+    static void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo);
+    static bool verifyValidationLayerSupport();
+    static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+        VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+        VkDebugUtilsMessageTypeFlagsEXT messageType,
+        const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+        void* pUserData
+    );
 
     void mainLoop();
 
@@ -24,4 +46,5 @@ private:
     GLFWwindow* mWindow;
 
     VkInstance mInstance;
+    VkDebugUtilsMessengerEXT mDebugMessenger;
 };
